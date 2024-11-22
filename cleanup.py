3c5fff5fe62b46a885202f98954ee41eeab3c790
Jaime <jaime.tellie@gmail.com>
2024-11-22 15:00:48 +0100

    Corrected cleanup script semantic logic

--- cleanup.py
+++ cleanup.py
@@ -12,21 +12,19 @@ def delete_database_instance(instance_name: str):
     deleted_database_instance = subprocess.check_output(
         ["gcloud", "sql", "instances", "delete", f"{instance_name}"]
     )
-    print(deleted_database_instance)
     logger.info("Database instance deleted!\n")
 
-async def delete_database(database_name: str, instance_name: str):
+def delete_database(database_name: str, instance_name: str):
     logger.info(f"Deleting database {database_name} from instance {instance_name}.")
     deleted_database = subprocess.check_output(
         ["gcloud", "sql", "databases", "delete", f"{database_name}", "--instance", f"{instance_name}"]
     )
-    print(deleted_database)
     logger.info("Database deleted!\n")
 
 
-async def main():
-    await delete_database_instance(DATABASE)
-    await delete_database(DATABASE, INSTANCE)
+def main():
+    delete_database(DATABASE, INSTANCE)
+    delete_database_instance(INSTANCE)
     
 
 if __name__=="__main__":
